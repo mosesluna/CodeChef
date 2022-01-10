2dd025013f3b26a13f206692010968dc06edab59
Moses <91290515+mosesluna@users.noreply.github.com>
2022-01-10 11:30:41 -0500

    Update CovidSpread.java

--- CovidSpread.java
+++ CovidSpread.java
@@ -2,7 +2,7 @@
  * Program Name: Covid Spread
  * Programmer: Moses
  * Date: January 8, 2022
- * Problem Link: https://www.codechef.com/JAN221C/problems/KEPLERSLAW 
+ * Problem Link: https://www.codechef.com/JAN221C/problems/COVSPRD
  * */
 package covidspread;
 
